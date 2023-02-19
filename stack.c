5bbbe7488b735a2fd353a5dfdac2ad3ddef80838
parkkrrr <prakharverma2019@gmail.com>
2023-02-19 15:21:59 +0530

    modified:   stack.c

--- stack.c
+++ stack.c
@@ -1,11 +1,29 @@
+#include <limits.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-void push();
-void pop();
-void top();
-void isEmpty();
-void size();
+// Source : https://www.geeksforgeeks.org/introduction-to-stack-data-structure-and-algorithm-tutorials/
+
+typedef struct stackNode
+{
+    int data;
+    struct stackNode *next;
+} node;
+
+void push(node **root, int data);
+int pop(node **root);
+int top(node *root);
+int isEmpty(node *root);
+// int size();
+
+
+node *newNode(int data)
+{
+    node *stackNode = (node *)malloc(sizeof(node));
+    stackNode->data = data;
+    stackNode->next = NULL;
+    return stackNode;
+}
 
 int main()
 {
@@ -15,36 +33,96 @@ int main()
     printf("2. Remove an Element\n");
     printf("3. Display top Element\n");
     printf("4. Check if Stack isEmpty\n");
-    printf("5. Size of the stack\n");
+    // printf("5. Size of the stack\n");
+    printf("6. Exit\n");
+    
     printf("--------------------------------");
 
     int option;
     scanf("%d", &option);
 
-    while (option != 6)
-    {
-        switch (option)
-        {
-        case '1':
-            push();
-            break;
-        case '2':
-            pop();
-            break;
-        case '3':
-            top();
-            break;
-        case '4':
-            isEmpty();
-            break;
-        case '5':
-            size();
-            break;
-        case '6':
-            break;
-        default:
-            printf("Invalid Option");
-        }
-    }
+    node *root = NULL;
+
+    push(&root, 10);
+    push(&root, 20);
+    push(&root, 30);
+
+    printf("%d popped from stack \n", pop(&root));
+
+    printf("Top element is %d \n", top(root));
+
+    // while (option != 6)
+    // {
+        // switch (option)
+        // {
+        // case '1':
+        //     push();
+        //     break;
+        // case '2':
+        //     pop();
+        //     break;
+        // case '3':
+        //     top();
+        //     break;
+        // case '4':
+        //     isEmpty();
+        //     break;
+        // // case '5':
+        // //     size();
+        // //     break;
+        // case '6':
+        //     break;
+        // default:
+        //     printf("Invalid Option");
+        // }
+    
+    // }
+
     return 0;
 }
+
+void push(node **root, int data) // n is the size of the stack
+{
+    // if(top ==n)
+    //     printf("\n Stack Overflow!!!");
+    // else {
+    //     top += 1;
+    //     stack[top]=val;
+    // }
+
+    node *stackNode = newNode(data);
+    stackNode->next = *root;
+    *root = stackNode;
+    printf("%d Pushed to stack \n", data);
+}
+int pop(node **root)
+{
+    // if(top == -1){
+    //     printf("Stack Underflow");
+    //     return 0;
+    // }
+    // else
+    //     return stack[top];
+
+    if (isEmpty(*root))
+        return INT_MIN;
+    node *temp = *root;
+    *root = (*root)->next;
+    int popped = temp->data;
+    free(temp);
+
+    return popped;
+}
+int top(node *root)
+{
+    if (isEmpty(root))
+        return INT_MIN;
+    return root->data;
+}
+int isEmpty(node *root)
+{
+    return !root;
+}
+// int size(){
+//     return top==;
+// }
